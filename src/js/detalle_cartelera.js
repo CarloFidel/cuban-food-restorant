03003186266485cd69fd3599f915881975d03ba5
CarloFidel <carloftaboada@gmail.com>
2026-06-03 18:54:43 +0200

    refact:refactoring image dynamic in detale_cartelera.js

--- src/js/detalle_cartelera.js
+++ src/js/detalle_cartelera.js
@@ -35,19 +35,32 @@ export function createDetalleCarteleraContent(dataCartelera) {
 
     const sourceMobil = document.createElement("source");
     sourceMobil.media = "(max-width: 600px)";
-    sourceMobil.srcset = `/mobil/${imgMobile}`;
+    sourceMobil.srcset = `/mobil/${imgMobile}?as=avif&width=800 1x, /mobil/${imgMobile}?as=avif&width=1600 2x`;
+    sourceMobil.type = "image/avif";
+
+    const sourceMobilWebp = document.createElement("source");
+    sourceMobilWebp.media = "(max-width: 600px)";
+    sourceMobilWebp.srcset = `/mobil/${imgMobile}?as=webp&width=800 1x, /mobil/${imgMobile}?as=webp&width=1600 2x`;
+    sourceMobilWebp.type = "image/webp";
 
     const sourceDesktop = document.createElement("source");
     sourceDesktop.media = "(min-width: 601px)";
-    sourceDesktop.srcset = `/${imgDesktop}`;
+    sourceDesktop.srcset = `/${imgDesktop}?as=avif&width=800 1x, /${imgDesktop}?as=avif&width=1600 2x`;
+    sourceDesktop.type = "image/avif";
+
+    const sourceDesktopWebp = document.createElement("source");
+    sourceDesktopWebp.media = "(min-width: 601px)";
+    sourceDesktopWebp.srcset = `/${imgDesktop}?as=webp&width=800 1x, /${imgDesktop}?as=webp&width=1600 2x`;
+    sourceDesktopWebp.type = "image/webp";
 
     const img = document.createElement("img");
     img.src = `/${imgDesktop}`;
     img.alt = titulo;
-    img.loading = "lazy";
 
     detallePicture.appendChild(sourceMobil);
+    detallePicture.appendChild(sourceMobilWebp);
     detallePicture.appendChild(sourceDesktop);
+    detallePicture.appendChild(sourceDesktopWebp);
     detallePicture.appendChild(img);
 
     tituloBanda.textContent = titulo;
@@ -55,3 +68,8 @@ export function createDetalleCarteleraContent(dataCartelera) {
     bandaVideo.setAttribute("src", video);
   }
 }
+
+{/* <source
+  srcset="/nuestros_chefs.49413da5.avif 1x,/nuestros_chefs.51686d85.avif 2x"
+  type="image/avif"
+></source>; */}
